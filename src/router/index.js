1759fb42f38385bf546e0e7f453ba240a11514b8
algoritmaAZ <dev.alfri@gmail.com>
2023-10-24 00:24:29 +0700

    fix route

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SoalView from '../views/SoalView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'soal',
+      component: SoalView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/kerjakan-soal',
+      name: 'kerjakan-soal',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/KerjakanView.vue')
     }
   ]
 })
