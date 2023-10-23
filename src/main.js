58d6bdeb774bc9526fa5fe183e79a65f94efd017
algoritmaAZ <dev.alfri@gmail.com>
2023-10-24 00:22:24 +0700

    add dependencies axios, bootstrap, bootstrap-icon

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import './assets/main.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import '/src/assets/main.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
