5176fadd2f71d97cec66eaaeb2e084560fc85ca4
algoritmaAZ <dev.alfri@gmail.com>
2023-11-06 20:46:06 +0700

    create function showalert

---
+++ src/utils/utils.js
@@ -0,0 +1,10 @@
+export const showAlert = ({ message, background }) => {
+  // TODO show alert one by one
+  const alert = document.getElementById('alert')
+  alert.innerHTML = `<div class="alert ${background} w-25 fade show" role="alert">
+    ${message}
+  </div>`
+  setTimeout(() => {
+    alert.innerHTML = ''
+  }, 2500)
+}
