535a213ec653ce7f63a1f7904d017f6bbee56d60
algoritmaAZ <dev.alfri@gmail.com>
2023-11-06 20:45:54 +0700

    rename soalview

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SoalView from '../views/SoalView.vue'
+import lessonView from '../views/lessonView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'soal',
-      component: SoalView
+      component: lessonView
     },
     {
       path: '/kerjakan-soal',
